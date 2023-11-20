f50e3fae205b90b1878efca32050fdcb9c40543a
sunrab1822 <wbarnus99@gmail.com>
2023-11-20 14:18:28 +0100

    trainings collection created

--- routes/trainings.js
+++ routes/trainings.js
@@ -1,23 +1,23 @@
 const express = require('express')
 const router = express.Router()
 router.get("/", (req, res) => {
-    res.status(200).json({ success: true, msg: "Show all courses" });
+    res.status(200).json({ success: true, msg: "Összes képzés" });
 });
 router.get("/:id", (req, res) => {
-    res.status(200).json({ success: true, msg: `Get course ${req.params.id}` });
+    res.status(200).json({ success: true, msg: ` ${req.params.id} id-vel rendelkezö képzés` });
 });
 router.post("/", (req, res) => {
-    res.status(200).json({ success: true, msg: "Create new course" });
+    res.status(200).json({ success: true, msg: "uj képzés ltrehozasa" });
 });
 router.put("/:id", (req, res) => {
     res
         .status(200)
-        .json({ success: true, msg: `Update course ${req.params.id}` });
+        .json({ success: true, msg: `kepzés frissitése ${req.params.id}` });
 });
 router.delete("/:id", (req, res) => {
     res
         .status(200)
-        .json({ success: true, msg: `Delete course ${req.params.id}` });
+        .json({ success: true, msg: ` képzés törlése ${req.params.id}` });
 });
 
 module.exports = router
