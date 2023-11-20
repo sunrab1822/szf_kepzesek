43f11256cea43f990448758f0f41d8bd8b6d8cbd
sunrab1822 <wbarnus99@gmail.com>
2023-11-20 14:32:34 +0100

    new files + better route

--- routes/trainings.js
+++ routes/trainings.js
@@ -1,24 +1,20 @@
-const express = require('express')
-const router = express.Router()
-router.get("/", (req, res) => {
-    res.status(200).json({ success: true, msg: "Összes képzés" });
-});
-router.get("/:id", (req, res) => {
-    res.status(200).json({ success: true, msg: ` ${req.params.id} id-vel rendelkezö képzés` });
-});
-router.post("/", (req, res) => {
-    res.status(200).json({ success: true, msg: "uj képzés ltrehozasa" });
-});
-router.put("/:id", (req, res) => {
-    res
-        .status(200)
-        .json({ success: true, msg: `kepzés frissitése ${req.params.id}` });
-});
-router.delete("/:id", (req, res) => {
-    res
-        .status(200)
-        .json({ success: true, msg: ` képzés törlése ${req.params.id}` });
-});
+const express = require("express");
+const {
+    getTraining,
+    getTrainings,
+    createTraining,
+    updateTraining,
+    deleteTraining,
+} = require("../controllers/trainings.js");
+const router = express.Router();
+router.route("/").get(getTrainings).post(createTraining);
+router
+    .route("/:id")
+    .get(getTraining)
+    .put(updateTraining)
+    .delete(deleteTraining);
 
-module.exports = router
+
+module.exports = router;
+
 
