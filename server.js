e03d8b23652449765bd7e60c986a3db5ec5be37a
sunrab1822 <wbarnus99@gmail.com>
2023-11-20 14:06:54 +0100

    routes to trainings works

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const trainings = require('./routes/trainings')
 
 
 require('dotenv').config() // A .env fájlt olvassa
 
 const app = express()
-//test fix
+
+app.use('/api/trainings', trainings)
 
 app.listen(process.env.PORT, ()=> { console.log(`Server running on port ${process.env.PORT}`)});
