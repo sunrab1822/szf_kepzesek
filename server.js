1d1954743d5c3721eecf507d6563e579e4b58398
sunrab1822 <wbarnus99@gmail.com>
2023-11-20 13:26:50 +0100

    test

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const express = require('express')
 require('dotenv').config() // A .env fájlt olvassa
 
 const app = express()
-
+//test fix
 
 app.listen(process.env.PORT, ()=> { console.log(`Server running on port ${process.env.PORT}`)});
